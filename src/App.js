57884924bb577a85cb614a8a73bff83eea5eb995
gasvel <gastonveliez95@gmail.com>
2019-03-24 01:31:46 -0300

    Agregado bootstrap y navbar simple

--- src/App.js
+++ src/App.js
@@ -6,6 +6,25 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <nav className="navbar navbar-dark bg-primary">
+          
+        <a className="navbar-brand" href="#">Navbar</a>
+          <ul className="nav mr-auto mt-2 mt-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" href="https://google.com">Home <span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Link</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#" >Disabled</a>
+            </li>
+          </ul>
+          <form className="form-inline my-2 my-lg-0">
+            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
+            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </form>
+        </nav>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
@@ -21,7 +40,7 @@ class App extends Component {
           </a>
         </header>
       </div>
-    );
+          );
   }
 }
 
